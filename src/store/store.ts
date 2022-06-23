6d2cf6c3e4757cc22db7af1655b0d3a41521d262
Daniel Koch <koch@terrestris.de>
2022-06-23 17:10:59 +0200

    feat: add appInfo to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,12 +3,14 @@ import {
 } from '@reduxjs/toolkit';
 
 import addLayerModal from './addLayerModal';
+import appInfo from './appInfo';
 import logoPath from './logoPath';
 import title from './title';
 import toolMenu from './toolMenu';
 
 export const store = configureStore({
   reducer: {
+    appInfo,
     title,
     logoPath,
     toolMenu,
