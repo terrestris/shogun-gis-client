6e460c2541505e068d40b647daf66207eb9d2003
Daniel Koch <koch@terrestris.de>
2022-06-23 17:12:04 +0200

    feat: add getGravatarUrl util

---
+++ src/utils/getGravatarUrl.ts
@@ -0,0 +1,33 @@
+import md5 from 'js-md5';
+
+export interface GravatarProps {
+  email: string;
+  size?: number;
+  rating?: 'g' | 'pg' | 'r' | 'x';
+  defaultImage?: '404' | 'mp' | 'identicon' | 'monsterid' | 'wavatar' | 'retro' | 'robohash' | 'blank';
+  force?: boolean;
+}
+
+const GRAVATAR_BASE_URL = 'https://www.gravatar.com/avatar/';
+
+/**
+ * https://de.gravatar.com/site/implement/images/
+ */
+export const getGravatarUrl = ({
+  email = '',
+  size = 80,
+  rating = 'g',
+  defaultImage = 'identicon',
+  force = false
+}: GravatarProps): string => {
+  const lowerCaseEmail = email.toLowerCase();
+  const hash = md5(lowerCaseEmail);
+
+  let url = `${GRAVATAR_BASE_URL}${hash}?s=${size}&r=${rating}&d=${defaultImage}`;
+
+  if (force) {
+    url = `${url}&f=y`;
+  }
+
+  return url;
+};
