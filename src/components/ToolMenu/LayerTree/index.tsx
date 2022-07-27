1fb6a85b9277e4534db5d2ebf31a3fd6f2d2b5a9
Fritz Hoeing <fhoeing@terrestris.de>
2022-07-27 12:13:23 +0200

    feat: implements the draggable icon from the library

--- src/components/ToolMenu/LayerTree/index.tsx
+++ src/components/ToolMenu/LayerTree/index.tsx
@@ -3,12 +3,6 @@ import React, {
 } from 'react';
 
 import {
-  faBars
-} from '@fortawesome/free-solid-svg-icons';
-import {
-  FontAwesomeIcon
-} from '@fortawesome/react-fontawesome';
-import {
   getUid
 } from 'ol';
 import OlBaseLayer from 'ol/layer/Base';
@@ -74,7 +68,6 @@ export const LayerTree: React.FC<LayerTreeProps> = ({
       return (
         <div>
           {layer.get('name')}
-          <FontAwesomeIcon icon={faBars} />
         </div>
       );
     } else {
@@ -126,9 +119,7 @@ export const LayerTree: React.FC<LayerTreeProps> = ({
       map={map}
       nodeTitleRenderer={treeNodeTitleRenderer}
       filterFunction={treeFilterFunction}
-      draggable={{
-        icon: false
-      }}
+      draggable
       {...restProps}
     />
   );
