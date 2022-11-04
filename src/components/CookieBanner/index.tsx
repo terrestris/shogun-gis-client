d1edea6d5e6c45a6f931806a44c830df4a73e905
Fritz Hoeing <fhoeing@terrestris.de>
2022-11-04 14:13:12 +0100

    feat: adds an entry-point

--- src/components/CookieBanner/index.tsx
+++ src/components/CookieBanner/index.tsx
@@ -1,28 +1,64 @@
 import React from 'react';
 
-import CookieConsent from 'react-cookie-consent';
+import {
+  CookieConsent
+} from 'react-cookie-consent';
 import { useTranslation } from 'react-i18next';
+
+import usePlugins from '../../hooks/usePlugins';
+import { isCookieConsentIntegration } from '../../plugin';
 
 import './index.less';
 
 export const CookieBanner = () => {
 
+  const plugins = usePlugins();
+
   const {
     t
   } = useTranslation();
 
+  const getPlugin = () => {
+    let cookiePlugin: React.ReactElement | null = null;
+
+    // TODO: warn if more than one cookie-plugin has been detected
+
+    plugins.forEach(plugin => {
+      if (isCookieConsentIntegration(plugin.integration)) {
+        const {
+          key,
+          wrappedComponent: WrappedPluginComponent
+        } = plugin;
+
+        cookiePlugin = (
+          <WrappedPluginComponent
+            key={key}
+          />
+        );
+      }
+    });
+
+    return cookiePlugin;
+  };
+
   return (
-    <CookieConsent
-      location="bottom"
-      buttonText={t('CookieBanner.button')}
-      cookieName="SHOGun-Session-Cookie"
-      buttonClasses='cookie-button'
-      containerClasses='cookie-container'
-      contentClasses='cookie-content'
-      expires={150}
+    <div
+      className='CookieBanner'
     >
-      {t('CookieBanner.info')}
-    </CookieConsent>
+      <CookieConsent
+        key='default-cookie-consent'
+        location="bottom"
+        buttonText={t('CookieBanner.button')}
+        cookieName="SHOGun-Session-Cookie"
+        buttonClasses='cookie-button'
+        containerClasses='cookie-container'
+        contentClasses='cookie-content'
+        expires={150}
+      >
+        {getPlugin() ? getPlugin() : t('CookieBanner.info')}
+      </CookieConsent>;
+    </div>
+
   );
 };
 
