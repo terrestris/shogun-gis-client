4b260c59c105870f1f8c5807bfd3a1c69b93d5ae
Michael Holthausen <mholthausen@terrestris.de>
2023-01-20 14:59:55 +0100

    chore: update tests of Footer component

--- src/components/Footer/index.spec.tsx
+++ src/components/Footer/index.spec.tsx
@@ -1,22 +1,21 @@
-
 import React from 'react';
 
 import {
-  render,
-  screen
+  render, screen
 } from '@testing-library/react';
 
 import OlMap from 'ol/Map';
 import OlView from 'ol/View';
 
-import {
-  renderInMapContext
-} from '@terrestris/react-geo/dist/Util/rtlTestUtils';
+import { Provider } from 'react-redux';
+
+import { renderInMapContext } from '@terrestris/react-geo/dist/Util/rtlTestUtils';
+
+import { store } from '../../store/store';
 
 import Footer from './index';
 
 describe('<Footer />', () => {
-
   const center = [829729, 6708850];
   let map: OlMap;
 
@@ -36,45 +35,67 @@ describe('<Footer />', () => {
   });
 
   it('can be rendered', () => {
-    const {
-      container
-    } = render(<Footer />);
+    const { container } = render(
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    );
     expect(container).toBeVisible();
   });
 
   it('contact is rendered', () => {
-    renderInMapContext(map, <Footer />);
+    renderInMapContext(
+      map,
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    );
     const contact = screen.getByText('Footer.contact');
     expect(contact).toBeVisible();
   });
 
   it('imprint is rendered', () => {
-    renderInMapContext(map, <Footer />);
+    renderInMapContext(
+      map,
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    );
     const imprint = screen.getByText('Footer.imprint');
     expect(imprint).toBeVisible();
   });
 
   it('privacy is rendered', () => {
-    renderInMapContext(map, <Footer />);
+    renderInMapContext(
+      map,
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    );
     const privacy = screen.getByText('Footer.privacypolicy');
     expect(privacy).toBeVisible();
   });
 
   it('mouseposition controls is rendered', () => {
-    const {
-      container
-    } = renderInMapContext(map, <Footer />);
+    const { container } = renderInMapContext(
+      map,
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    );
     const mousePosition = container.querySelector('div.ol-mouse-position');
     expect(mousePosition).toBeVisible();
   });
 
   it('scalebar control is rendered', () => {
-    const {
-      container
-    } = renderInMapContext(map, <Footer />);
+    const { container } = renderInMapContext(
+      map,
+      <Provider store={store}>
+        <Footer />
+      </Provider>
+    );
 
     const scalebar = container.querySelector('div.ol-scale-line');
     expect(scalebar).toBeVisible();
   });
-
 });
