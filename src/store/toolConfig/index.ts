282393b67f619a171b355af09594f5a9bfe083e1
Daniel Koch <koch@terrestris.de>
2023-01-25 14:57:34 +0100

    feat: add typing for the tool configurations and add actions to manipulate draw style and features

--- src/store/toolConfig/index.ts
+++ src/store/toolConfig/index.ts
@@ -4,8 +4,8 @@ import {
 } from '@reduxjs/toolkit';
 
 import {
-  DefaultApplicationToolConfig
-} from '@terrestris/shogun-util/dist/model/Application';
+  Style
+} from 'geostyler-style';
 
 // eslint-disable-next-line no-shadow
 export enum ClientTools {
@@ -19,14 +19,76 @@ export enum ClientTools {
   LANGUAGE_SELECTOR = 'language_selector'
 };
 
-const initialState: DefaultApplicationToolConfig[] = [{
-  name: ClientTools.MEASURE_TOOLS,
+export type BaseToolConfig<T extends ClientTools, S = {}> = {
+  name: T;
   config: {
-    visible: true,
-    showMeasureDistance: true,
-    showMeasureArea: true
+    visible: boolean;
+  } & S;
+};
+
+export type MeasureToolConfig = BaseToolConfig<
+  ClientTools.MEASURE_TOOLS,
+  {
+    showMeasureDistance: boolean;
+    showMeasureArea: boolean;
   }
-}, {
+>;
+
+export type DrawToolConfig = BaseToolConfig<
+  ClientTools.DRAW_TOOLS,
+  {
+    showDrawPoint?: boolean;
+    showDrawLine?: boolean;
+    showDrawPolygon?: boolean;
+    showDrawCircle?: boolean;
+    showDrawRectangle?: boolean;
+    showDrawAnnotation?: boolean;
+    showModifyFeatures?: boolean;
+    showUploadFeatures?: boolean;
+    showDownloadFeatures?: boolean;
+    showDeleteFeatures?: boolean;
+    showStyleEditor?: boolean;
+    features?: GeoJSON.FeatureCollection;
+    style?: Style;
+  }
+>;
+
+export type FeatureInfoToolConfig = BaseToolConfig<
+  ClientTools.FEATURE_INFO
+>;
+
+export type PrintToolConfig = BaseToolConfig<
+  ClientTools.PRINT
+>;
+
+export type TreeToolConfig = BaseToolConfig<
+  ClientTools.TREE
+>;
+
+export type LanguageSelectorToolConfig = BaseToolConfig<
+  ClientTools.LANGUAGE_SELECTOR
+>;
+
+export type PermalinkToolConfig = BaseToolConfig<
+  ClientTools.PERMALINK
+>;
+
+export type AppContextToolConfig = BaseToolConfig<
+  ClientTools.APP_CONTEXT
+>;
+
+export type ApplicationToolConfig = Array<
+  MeasureToolConfig |
+  DrawToolConfig |
+  FeatureInfoToolConfig |
+  PrintToolConfig |
+  TreeToolConfig|
+  LanguageSelectorToolConfig |
+  PermalinkToolConfig |
+  AppContextToolConfig
+>;
+
+const initialState: ApplicationToolConfig = [{
   name: ClientTools.DRAW_TOOLS,
   config: {
     visible: true,
@@ -41,9 +103,72 @@ const initialState: DefaultApplicationToolConfig[] = [{
     showDownloadFeatures: true,
     showDeleteFeatures: true,
     showStyleEditor: true,
-    features: null,
-    // TODO Provide the default style here?
-    style: null
+    features: {
+      type: 'FeatureCollection',
+      features: []
+    },
+    style: {
+      name: 'Default Style',
+      rules: [{
+        name: 'Area',
+        symbolizers: [{
+          kind: 'Fill',
+          color: '#00b72b',
+          outlineOpacity: 0.8,
+          opacity: 0.5,
+          fillOpacity: 0.8,
+          outlineWidth: 2,
+          outlineColor: '#00b72b'
+        }]
+      }, {
+        name: 'Line',
+        symbolizers: [{
+          kind: 'Line',
+          color: '#00b72b',
+          width: 2,
+          opacity: 0.8
+        }]
+      }, {
+        name: 'Point',
+        symbolizers: [{
+          kind: 'Mark',
+          wellKnownName: 'circle',
+          color: '#00b72b',
+          strokeColor: '#00b72b',
+          strokeOpacity: 0.8,
+          opacity: 0.5,
+          radius: 7
+        }],
+        filter: [
+          '==',
+          'label',
+          'undefined'
+        ]
+      }, {
+        name: 'Text',
+        symbolizers: [{
+          kind: 'Text',
+          label: '{{label}}',
+          size: 12,
+          font: [
+            'sans-serif'
+          ],
+          opacity: 0.8,
+          color: '#00b72b',
+          offset: [
+            5,
+            5
+          ],
+          haloColor: '#00b72b',
+          haloWidth: 1
+        }],
+        filter: [
+          '!=',
+          'label',
+          'undefined'
+        ]
+      }]
+    }
   }
 }, {
   name: ClientTools.FEATURE_INFO,
@@ -81,14 +206,94 @@ export const slice = createSlice({
   name: 'toolConfig',
   initialState,
   reducers: {
-    setToolConfig(state, action: PayloadAction<DefaultApplicationToolConfig[]>) {
+    setToolConfig(state, action: PayloadAction<ApplicationToolConfig>) {
       return action.payload;
+    },
+    setDrawFeatures: (state, action: PayloadAction<GeoJSON.FeatureCollection>) => {
+      const drawToolConfigIdx = state.findIndex(config => config.name === ClientTools.DRAW_TOOLS);
+
+      if (drawToolConfigIdx === -1) {
+        return;
+      }
+
+      (state[drawToolConfigIdx] as DrawToolConfig).config.features = action.payload;
+    },
+    addDrawFeature: (state, action: PayloadAction<GeoJSON.Feature>) => {
+      const drawToolConfigIdx = state.findIndex(config => config.name === ClientTools.DRAW_TOOLS);
+
+      if (drawToolConfigIdx === -1) {
+        return;
+      }
+
+      (state[drawToolConfigIdx] as DrawToolConfig).config.features?.features.push(action.payload);
+    },
+    addDrawFeatures: (state, action: PayloadAction<GeoJSON.FeatureCollection>) => {
+      const drawToolConfigIdx = state.findIndex(config => config.name === ClientTools.DRAW_TOOLS);
+
+      if (drawToolConfigIdx === -1) {
+        return;
+      }
+
+      (state[drawToolConfigIdx] as DrawToolConfig).config.features?.features.push(...action.payload.features);
+    },
+    updateDrawFeature: (state, action: PayloadAction<GeoJSON.Feature>) => {
+      const drawToolConfigIdx = state.findIndex(config => config.name === ClientTools.DRAW_TOOLS);
+
+      if (drawToolConfigIdx === -1) {
+        return;
+      }
+
+      const updateCandidateIdx = (state[drawToolConfigIdx] as DrawToolConfig).config.features?.features
+        .findIndex((feat: GeoJSON.Feature) => feat.id === action.payload.id);
+
+      if (updateCandidateIdx === undefined || updateCandidateIdx === -1) {
+        return;
+      }
+
+      const featuresToUpdate = (state[drawToolConfigIdx] as DrawToolConfig).config.features;
+
+      if (!featuresToUpdate) {
+        return;
+      }
+
+      featuresToUpdate.features[updateCandidateIdx] = action.payload;
+    },
+    removeDrawFeature: (state, action: PayloadAction<string | number>) => {
+      const drawToolConfigIdx = state.findIndex(config => config.name === ClientTools.DRAW_TOOLS);
+
+      if (drawToolConfigIdx === -1) {
+        return;
+      }
+
+      const updateCandidateIdx = (state[drawToolConfigIdx] as DrawToolConfig).config.features?.features
+        .findIndex((feat: GeoJSON.Feature) => feat.id === action.payload);
+
+      if (updateCandidateIdx === undefined || updateCandidateIdx === -1) {
+        return;
+      }
+
+      (state[drawToolConfigIdx] as DrawToolConfig).config.features?.features.splice(updateCandidateIdx, 1);
+    },
+    setDrawStyle: (state, action: PayloadAction<Style>) => {
+      const drawToolConfigIdx = state.findIndex(config => config.name === ClientTools.DRAW_TOOLS);
+
+      if (drawToolConfigIdx === -1) {
+        return;
+      }
+
+      (state[drawToolConfigIdx] as DrawToolConfig).config.style = action.payload;
     }
   }
 });
 
 export const {
-  setToolConfig
+  setToolConfig,
+  setDrawFeatures,
+  addDrawFeature,
+  addDrawFeatures,
+  updateDrawFeature,
+  removeDrawFeature,
+  setDrawStyle
 } = slice.actions;
 
 export default slice.reducer;
