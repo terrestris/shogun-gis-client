cb07666616afb9682811206cf95ea6dc1ef054bc
Fritz Hoeing <fhoeing@terrestris.de>
2023-03-22 15:53:33 +0100

    fix: fixing authentication

--- global-setup.js
+++ global-setup.js
@@ -1,6 +1,6 @@
 async function globalSetup() {
-  process.env.ID = '20';
-  process.env.HOST = 'https://shogun2022.intranet.terrestris.de';
+  process.env.ID = '21';
+  process.env.HOST = 'shogun2022.intranet.terrestris.de';
 }
 
 export default globalSetup;
--- src/e2e-tests/auth.setup.ts
+++ src/e2e-tests/auth.setup.ts
@@ -6,10 +6,10 @@ const adminFile = 'playwright/.auth/admin.json';
 
 setup('authenticate as admin', async ({ page }) => {
 
-  await page.goto(`${process.env.HOST}/auth/realms/SHOGun/protocol/openid-connect/auth?`
-  + `client_id=shogun-client&redirect_uri=https%3A%2F%2F${process.env.HOST}%2Fclient%2F&`
-  + 'state=fb97b06e-b0fa-4ce2-8a37-30d31e45ab0b&response_mode=fragment&response_type=code'
-  + '&scope=openid&nonce=d23e668a-2dc9-48cf-8b22-473d0580dee0');
+  await page.goto(`https://${process.env.HOST}/auth/realms/SHOGun/protocol/openid-connect/auth?`
+  + `client_id=shogun-client&redirect_uri=https%3A%2F%2F${process.env.HOST}%2Fclient%2F&state=`
+  + 'f9c5c223-1028-4496-8fdb-ae1da0f5daba&response_mode=fragment&response_type='
+  + 'code&scope=openid&nonce=6cd0da9a-b45c-4a9e-931d-99b6725a148e');
 
   await page.getByLabel('Username or email').fill(`${process.env.ADMIN_LOGIN}`);
   await page.getByLabel('Password').fill(`${process.env.ADMIN_PASSWORD}`);
