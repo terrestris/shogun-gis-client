01bb67e7afc7b7efb9436e1b63d800bcbb45f9fd
sdobbert <132580338+sdobbert@users.noreply.github.com>
2024-05-22 16:03:04 +0200

    fix: minor fix

--- src/store/editFeature/index.ts
+++ src/store/editFeature/index.ts
@@ -42,7 +42,7 @@ const editFeatureSlice = createSlice({
       state.feature = null;
     },
     setUserEditMode(state, action: PayloadAction<EditLevel[]>) {
-      state. = action.payload;
+      state.userEditMode = action.payload;
     },
     setFormDirty(state, action: PayloadAction<boolean>) {
       state.formDirty = action.payload;
