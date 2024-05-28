94d7b612a415918d6049a738d43c5b6f6f4a3ae7
sdobbert <dobbert@terrestris.de>
2024-05-28 14:02:37 +0200

    test: unittest for ApplicationInfo

--- src/components/ApplicationInfo/index.spec.tsx
+++ src/components/ApplicationInfo/index.spec.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import {
-  render
+  act,
+  fireEvent,
+  render,
+  screen
 } from '@testing-library/react';
 
 import { createReduxWrapper } from '../../utils/testUtils';
@@ -9,18 +12,57 @@ import { createReduxWrapper } from '../../utils/testUtils';
 import ApplicationInfo from './index';
 
 describe('<ApplicationInfo />', () => {
+  let modalElem: HTMLElement | null;
+
+  beforeEach(async () => {
+    const {
+      container
+    } = render(
+      <ApplicationInfo
+        opener={
+          <span
+            className="info-opener"
+            aria-label='info-opener'
+          >
+            {'UserMenu.infoMenuTitle'}
+          </span>
+        }
+      />,
+      {
+        wrapper: createReduxWrapper()
+      });
+    expect(container).toBeVisible();
+
+    const infoButton: Element | null = container.querySelector('.info-opener');
+    expect(infoButton).toBeVisible();
+
+    act(() => {
+      fireEvent.click(infoButton!);
+    });
+
+    modalElem = document.querySelector('.application-info');
+  });
 
   it('is defined', () => {
     expect(ApplicationInfo).not.toBeUndefined();
   });
 
-  it('can be rendered', () => {
-    const {
-      container
-    } = render(<ApplicationInfo />, {
-      wrapper: createReduxWrapper()
-    });
-    expect(container).toBeVisible();
+  it('modal is rendered', () => {
+    expect(modalElem).toBeVisible();
   });
 
+  it('logo is visible', () => {
+    const logoElem = document.querySelector('.logo');
+    expect(logoElem).toBeVisible();
+  });
+
+  it('version is visible', () => {
+    const versionTitleElem = screen.getByText('ApplicationInfo.clientVersionTitle');
+    expect(versionTitleElem).toBeVisible();
+
+    const version = global.PROJECT_VERSION;
+
+    const versionElem = screen.getByText(version.toString());
+    expect(versionElem).toBeVisible();
+  });
 });
