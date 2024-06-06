e8e8b3041fc8711e82194c68847a25435154dad3
sdobbert <dobbert@terrestris.de>
2024-06-06 13:43:07 +0200

    test:  unittest for EditRederenceTable

---
+++ src/components/EditFeatureDrawer/EditReferenceTable/index.spec.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import {
+  fireEvent,
+  render,
+  waitFor
+} from '@testing-library/react';
+
+import { createReduxWrapper } from '../../../utils/testUtils';
+
+import EditReferenceTable, { EditReferenceDataType } from '.';
+
+let tableElem: HTMLElement | null;
+let mockParentForm;
+
+describe('<EditReferenceTable />', () => {
+  it('is defined', () => {
+    expect(EditReferenceTable).toBeDefined();
+  });
+
+  beforeEach(() => {
+    const { container } =
+      render(
+        <EditReferenceTable
+          parentForm={mockParentForm}
+          propertyName={'test'}
+        />,
+        {
+          wrapper: createReduxWrapper()
+        });
+    tableElem = container;
+  });
+
+  it('can be rendered', () => {
+    expect(tableElem).toBeVisible();
+  });
+
+  it('add modal can be opened', async () => {
+    const buttonElem: HTMLElement | null = document.querySelector('.add-reference-button');
+    expect(buttonElem).toBeVisible();
+
+    await waitFor(() => {
+      fireEvent.click(buttonElem!);
+    });
+    const modalElem: HTMLElement | null = document.querySelector('.edit-reference-table-modal');
+    expect(modalElem).toBeVisible();
+  });
+});
