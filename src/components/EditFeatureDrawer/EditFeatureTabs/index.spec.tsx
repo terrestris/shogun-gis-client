7661ea116fdf1628b557367fa6ea929ba00416c8
sdobbert <dobbert@terrestris.de>
2024-06-06 11:43:22 +0200

    test: update unittest for EditFeatureTabs

--- src/components/EditFeatureDrawer/EditFeatureTabs/index.spec.tsx
+++ src/components/EditFeatureDrawer/EditFeatureTabs/index.spec.tsx
@@ -1,4 +1,18 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { FormInstance } from 'antd';
+
+import { PropertyFormItemEditConfig, PropertyFormTabConfig } from '@terrestris/shogun-util/dist/model/Layer';
+
+import { createReduxWrapper } from '../../../utils/testUtils';
+
 import EditFeatureTabs from '.';
+
+let mockTabConfig: PropertyFormTabConfig<PropertyFormItemEditConfig>[] | undefined;
+let mockForm: FormInstance<any>
+let mockProperties: { [name: string]: any; }
 
 describe('<EditFeatureTabs />', () => {
 
@@ -6,4 +20,17 @@ describe('<EditFeatureTabs />', () => {
     expect(EditFeatureTabs).toBeDefined();
   });
 
+  it('can be rendered', () => {
+    const { container } =
+      render(
+        <EditFeatureTabs
+          tabConfig={mockTabConfig}
+          form={mockForm}
+          initialValues={mockProperties} />,
+        {
+          wrapper: createReduxWrapper()
+        });
+
+    expect(container).toBeVisible();
+  });
 });
