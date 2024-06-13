0081f43e14536e4f56d9a8dbddbc21930bed1018
sdobbert <dobbert@terrestris.de>
2024-06-13 15:01:48 +0200

    test: update unittest for Header

--- src/components/Header/index.spec.tsx
+++ src/components/Header/index.spec.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import {
-  render
+  render,
+  screen
 } from '@testing-library/react';
 
+import { setLogoPath } from '../../store/logoPath';
+import { store } from '../../store/store';
+import { setTitle } from '../../store/title';
 import { createReduxWrapper } from '../../utils/testUtils';
 
 import Header from './index';
@@ -23,4 +27,60 @@ describe('<Header />', () => {
     expect(container).toBeVisible();
   });
 
+  it('can be rendered', () => {
+    const {
+      container
+    } = render(<Header />, {
+      wrapper: createReduxWrapper()
+    });
+    expect(container).toBeVisible();
+  });
+
+  it('logo is rendered', () => {
+    store.dispatch(setLogoPath('test-path'));
+
+    render(<Header />, {
+      wrapper: createReduxWrapper()
+    });
+
+    const logoElem = screen.getByLabelText('logo');
+
+    expect(logoElem).toBeVisible();
+    expect(logoElem).toHaveAttribute('src', 'test-path');
+  });
+
+  it('title is visible', () => {
+    store.dispatch(setTitle('test-title'));
+
+    const {
+      container
+    } = render(<Header />, {
+      wrapper: createReduxWrapper()
+    });
+    expect(container).toBeVisible();
+
+    const titleElem = container.querySelector('.title');
+
+    expect(titleElem).toHaveTextContent('test-title');
+  });
+
+  it('search field is rendered', () => {
+    render(<Header />, {
+      wrapper: createReduxWrapper()
+    });
+
+    const searchElem = screen.getByLabelText('search-field');
+
+    expect(searchElem).toBeVisible();
+  });
+
+  it('user menu is rendered', () => {
+    render(<Header />, {
+      wrapper: createReduxWrapper()
+    });
+
+    const menuElem = screen.getByLabelText('user-menu');
+
+    expect(menuElem).toBeVisible();
+  });
 });
