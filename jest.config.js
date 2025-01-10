46a950a826c92cdeed642461de16b909477b7c58
Andre Henn <henn@terrestris.de>
2025-01-10 14:33:29 +0100

    chore: latest updates especially shogun-util and module federation

--- jest.config.js
+++ jest.config.js
@@ -24,7 +24,7 @@ module.exports = {
   testEnvironment: './jest/CustomTestEnvironment.js',
   transformIgnorePatterns: [
     '<rootDir>/node_modules/(?!(ol|antd|@babel|jest-runtime|(rc-*[a-z]*)|@ant-design|@terrestris|color-*[a-z]*|@camptocamp|d3-*[a-z]*|' +
-    'query-string|decode-uri-component|strict-uri-encode|split-on-first|filter-obj|shpjs|geostyler))'
+    'query-string|decode-uri-component|strict-uri-encode|split-on-first|filter-obj|shpjs|geostyler|pbf))'
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
