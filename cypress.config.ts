d9dc6a7cb9481f14a90b5aa5e662b93ca72ec0e4
sdobbert <dobbert@terrestris.de>
2025-02-19 12:06:01 +0100

    ci: run cypress tests in ci

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,13 +5,15 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    }
+    },
+    chromeWebSecurity: false
   },
   env: {
+    ENV: process.env.ENV,
     HOST: process.env.HOST,
     ID: process.env.ID,
     ADMIN_LOGIN: process.env.ADMIN_LOGIN,
     ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
-    TITLE: 'SHOGun Demo-Client'
   },
 });
+
--- cypress/e2e/header.cy.ts
+++ cypress/e2e/header.cy.ts
@@ -1,19 +1,26 @@
 describe('Header', () => {
   beforeEach(() => {
-    cy.loginAsAdmin();
-    cy.visit(`https://${Cypress.env('HOST')}/client/?applicationId=${Cypress.env('ID')}`);
+    if (Cypress.env('ENV') == 'dev') {
+      cy.visit(`http://${Cypress.env('HOST')}/`);
+    } else {
+      cy.loginAsAdmin();
+      cy.visit(`https://${Cypress.env('HOST')}/client/?applicationId=${Cypress.env('ID')}`);  
+    }
     cy.get('button[aria-label="Accept cookies"]', { timeout: 10000 })
       .should('be.visible')
       .click();
     cy.get('#map', { timeout: 10000 })
       .should('be.visible')
-    cy.contains(Cypress.env('TITLE'));
   });
 
   it('should have a complete header', () => {
     cy.get('[aria-label="logo"]').should('be.visible');
     cy.get('[aria-label="search-field"]').should('be.visible');
-    cy.get('[aria-label="user-menu"]').should('be.visible');
+    cy.get('[aria-label="documentation-button"]').should('be.visible');
+
+    if (Cypress.env('ENV') == 'dev') {
+      cy.get('[aria-label="user-menu"]').should('be.visible');
+    }
   });
 });
 
