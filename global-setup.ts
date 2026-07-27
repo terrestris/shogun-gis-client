209e3f7fc7f6c09b699ccca83ebb032ad721c7c4
sdobbert <sdobbert@terrestris.de>
2026-07-27 10:39:33 +0200

    fix: remove default credentials

--- global-setup.ts
+++ global-setup.ts
@@ -2,11 +2,14 @@ import {
     chromium, FullConfig
 } from '@playwright/test';
 
+const DEFAULT_HOST = 'https://localhost:8080';
+
+
 async function globalSetup(config: FullConfig) {
     process.env.ID = '21';
-    process.env.HOST = 'https://shogun.intranet.terrestris.de';
-    process.env.ADMIN_LOGIN = 'shogun';
-    process.env.ADMIN_PASSWORD = 'shogun';
+    process.env.HOST = process.env.HOST ?? DEFAULT_HOST;
+    process.env.ADMIN_LOGIN =  process.env.ADMIN_LOGIN;
+    process.env.ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 }
 
 
