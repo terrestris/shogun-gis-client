3029a7a8f17c0d5fee55c56a533ae23928688421
sdobbert <sdobbert@terrestris.de>
2026-07-30 09:42:01 +0200

    fix: update id for e2e-tests

--- global-setup.ts
+++ global-setup.ts
@@ -3,10 +3,10 @@ import {
 } from '@playwright/test';
 
 const DEFAULT_HOST = 'https://localhost:8080';
-
+const DEFAULT_ID = '21';
 
 async function globalSetup(config: FullConfig) {
-    process.env.ID = '21';
+    process.env.ID = process.env.ID ?? DEFAULT_ID;
     process.env.HOST = process.env.HOST ?? DEFAULT_HOST;
     process.env.ADMIN_LOGIN =  process.env.ADMIN_LOGIN;
     process.env.ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
